072a9ce5dd8f9369d95fa0123ea490dee8b50a67
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-01-11 10:23:39 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
