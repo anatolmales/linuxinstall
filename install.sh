292f12e478e4879769d128ae5d5be05dcbd682ee
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-03-06 11:17:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 #sudo dpkg-reconfigure locales
 sudo apt install tmux zsh mc sudo python3-neovim -y
+#install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+wget -O ~/.zshrc.conf https://raw.githubusercontent.com/anatolmales/linuxinstall/main/zshrc
 
 #install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -13,11 +16,8 @@ sudo mv nvim.appimage /opt/nvim/nvim
 chmod u+x /opt/nvim/nvim
 
 echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
-wget -O ~/.zshrc.conf https://raw.githubusercontent.com/anatolmales/linuxinstall/main/zshrc
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/anatolmales/linuxinstall/main/tmux.conf
 
 #install config nvim
 git clone https://github.com/anatolmales/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
-#install omz
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
