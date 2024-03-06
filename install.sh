ed812f6b41b54a6fec007a2480360d129301e790
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-03-06 09:59:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,15 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mkdir -p /opt/nvim
+sudo mv nvim.appimage /opt/nvim/nvim
+sudo apt-get install python3-neovim
+echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
+
+
+#install config nvim
+git clone https://github.com/anatolmales/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
